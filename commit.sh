6422bdd5ff03ff861531f26ec9ac4c8388e3f61e
RodrigoProjects <RodrigoProjects>
2020-01-29 22:59:14 +0000

    General Commit | 2020-01-29 22:59

---
+++ commit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+now="$(date +%F) $(date +%R)"
+
+if [ -z "$1" ]
+then 
+    if [[ `git status --porcelain --untracked-files=no` ]]; then
+        git commit . -m "General Commit | $now"
+        git push
+    else 
+        printf "Ficheiros estão atualizados.\n(Nothing to commit)\n"
+    fi
+else
+    if [[ `git status --porcelain --untracked-files=no` ]]; then
+        git commit . -m "$1 | $now"
+        git push
+    else 
+        printf "Ficheiros estão atualizados.\n(Nothing to commit)\n"
+    fi
+fi
---
+++ desk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /mnt/c/Users/rodip/Desktop
+exec bash
